20b7dc25b3a99d0ea24796408ed8c267bcd40f34
Idrees Zamil Yaser <idrees.zamil@edu.karelia.com>
2024-01-03 16:57:44 +0200

    created the inputs

--- code.js
+++ code.js
@@ -1,7 +1,36 @@
 let gameName = "Guess The World";
 
 document.title = gameName;
-document.querySelector('h1').inertia = gameName
+document.querySelector("h1").inertia = gameName;
 
-document.querySelector('footer').innerHTML = `${gameName} Game created by Yaser`;
+document.querySelector(
+  "footer"
+).innerHTML = `${gameName} Game created by Yaser`;
 
+let numberoftries = 6;
+let numberofletters = 6;
+let currentTry = 1;
+
+function generateinput() {
+  const inputcontainer = document.querySelector(".inputs");
+  for (let i = 1; i < numberoftries; i++) {
+    let tryDiv = document.createElement("div");
+    tryDiv.classList.add(`try-${i}`);
+    tryDiv.innerHTML = `<span>try${i}</span>`;
+    if (i !== 1) tryDiv.classList.add('dosabled-inputs');
+    for (let j = 1; j < numberofletters; j++){
+      let input = document.createElement("input");
+      input.type = "text";
+      input.id = `try-${i}-letter-${j}`;
+      input.setAttribute("maxlength", "1");
+      tryDiv.appendChild(input);
+    }
+     inputcontainer.appendChild(tryDiv);
+  }
+inputcontainer.children[0].children[1].focus();
+
+}
+
+window.onload = function () {
+  generateinput();
+};
