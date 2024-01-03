60946f0853f4b28fc8f7964fad72f1b93b791e77
Idrees Zamil Yaser <idrees.zamil@edu.karelia.com>
2024-01-03 09:21:56 +0200

    the default rules

---
+++ code.js
@@ -0,0 +1,7 @@
+let gameName = "Guess The World";
+
+document.title = gameName;
+document.querySelector('h1').inertia = gameName
+
+document.querySelector('footer').innerHTML = `${gameName} Game created by Yaser`;
+
